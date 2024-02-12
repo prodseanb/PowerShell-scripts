71fb4df5985f8d949b1ac216130476056bd0322a
Sean Bachiller <59718043+prodseanb@users.noreply.github.com>
2024-02-12 10:43:12 -0500

    Update offboard.ps1 fixed

--- offboard.ps1
+++ offboard.ps1
@@ -1,26 +1,31 @@
 <# Use this script for offboarding users.
-Consider adding a feature to offboard multiple users (maybe from csv).
+Consider adding an option for multiple users
 @Authors: Sean Bachiller, Rabiya Amodwala
 #>
 
 function main {
-$User1 = Read-Host "Enter the USERNAME of the person to offboard:"
-    if (@(Get-ADUser -Filter { SamAccountName -eq $User1 }).Count -eq 0) {
-        Write-Warning -Message "User $User1 does not exist."
+    $User = Read-Host "Enter the USERNAME of the person to offboard"
+    try {
+    #Verify user exists
+        $adUser = Get-ADUser -Identity $User -ErrorAction Stop
+    #Hide from address lists - this is conditional since I don't have Exchange schemas to test
+        if (Get-ADObject -Filter 'lDAPDisplayName -like "msExchHideFromAddressLists"' -SearchBase (Get-ADRootDSE).schemaNamingContext) {
+        $adUser | Set-ADUser -Replace @{msExchHideFromAddressLists=$true}
     }
-    else {
-    #Remove manager 
-    Set-ADUser -Identity $User1 -Clear manager
+    #Clear manager
+        $adUser | Set-ADUser -Clear manager
+        $adUser | Set-ADUser -Enabled $false
     #Remove groups
-    $keep = @('CN=Domain Users,CN=Users,DC=sbllc,DC=org') #change
-    $groups.Where({$_ -notin ($keep)}) |
-    % { Remove-ADGroupMember -Identity $_ -Members $User1 }
-    #Hide from address lists
-    Set-ADObject -Identity $User1 -Replace @{msExchHideFromAddressLists=$true}
-    #Disable account
-    Set-ADUser -Identity $User1 -Enabled $false
-    exit
+        $keep = 'CN=Domain Users,CN=Users,DC=sbllc,DC=org' #change
+        $groups = Get-ADUser -Identity $User -properties memberof | select -expand memberof
+        $groups.Where({$_ -notin ($keep)}) |
+        % { Remove-ADGroupMember -Identity $_ -Members $User -Confirm:$false } 
+    #Move to Offboarded OU
+        $adUser | Move-ADObject -TargetPath "OU=Offboarded,DC=sbllc,DC=org"
+        Write-Host -ForegroundColor Green "Successfully offboarded $User"
+        pause
+    } catch {
+        throw
     }
 }
 main
-
